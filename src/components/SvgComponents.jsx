331721f2bdfd85715333e743f1a09c37086becf2
LSYbuilds <130975837+LSYbuilds@users.noreply.github.com>
2026-09-22 17:47:03 +0900

    svg 컴포넌트 생성

---
+++ src/components/SvgComponents.jsx
@@ -0,0 +1,15 @@
+import Proxy from "../../assets/svg/Proxy_icon.svg?react";
+import Public from "../../assets/svg/Proxy_icon.svg?react";
+import User from "../../assets/svg/Proxy_icon.svg?react";
+import Private from "../../assets/svg/Proxy_icon.svg?react";
+import ArrowRight from "../../assets/svg/Proxy_icon.svg?react";
+
+const Icon = {
+  proxy: Proxy,
+  public: Public,
+  user: User,
+  private: Private,
+  arrowRight: ArrowRight,
+};
+
+export default Icon;
